fce334fb044ee428e25ccebe718c54496facc37e
ItsMeSamey <sameychain5041@gmail.com>
2025-03-26 16:53:32 +0530

    optimal implementation for levenshtein_osa_distance as well

---
+++ heuristics/levenshtein_osa_distance.go
@@ -0,0 +1,64 @@
+package heuristics
+
+// Calculates the Optimal String Alignment (OSA) distance between two strings using a space-optimized approach.
+// Implementation adapted from https://wikipedia.org/wiki/Damerau-Levenshtein_distance
+func LevenshteinOSADistance(a string, b string) int {
+  // So the size of v0 and v1 are minimized
+  if len(a) < len(b) { a, b = b, a }
+
+  if len(b) < 2 { return LevenshteinDistance(a, b) }
+
+  // create two work vectors of integer distances
+  v0 := make([]int, len(b)+1)
+  v1 := make([]int, len(b)+1)
+  v2 := make([]int, len(b)+1)
+
+  for i := range len(b)+1 { v0[i] = i }
+
+  // Fill v1 with first pass of `LevenshteinDistance`
+  v1[0] = 1
+  for j := range b {
+    increment := 0
+    if a[0] != b[j] { increment = 1 }
+
+    v1[j+1] = min(
+      v0[j+1] + 1, // deletion cost
+      v1[j] + 1, // insertion cost
+      v0[j] + increment, // substitution cost
+    )
+  }
+
+  for i := 1; i < len(a); i += 1 {
+    v2[0] = i + 1
+
+    {
+      increment := 0
+      if a[i] != b[0] { increment = 1 }
+
+      v2[0+1] = min(
+        v1[1] + 1, // deletion cost
+        v2[0] + 1, // insertion cost
+        v1[0] + increment, // substitution cost
+      )
+    }
+
+    for j := 1; j < len(b); j += 1 {
+      increment := 0
+      if a[i] != b[j] { increment = 1 }
+
+      v2[j+1] = min(
+        v1[j+1] + 1, // deletion cost
+        v2[j] + 1, // insertion cost
+        v1[j] + increment, // substitution cost
+      )
+      if a[i] == b[j-1] && a[i-1] == b[j] {
+        v2[j+1] = min(v2[j+1], v0[j-1] + 1) // transposition
+      }
+    }
+
+    v0, v1, v2 = v1, v2, v0
+  }
+
+  return v1[len(b)]
+}
+
