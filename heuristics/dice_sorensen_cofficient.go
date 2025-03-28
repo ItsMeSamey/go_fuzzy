0c0529c19f3d796917707bb10241fd363e0484d0
ItsMeSamey <sameychain5041@gmail.com>
2025-03-28 12:53:30 +0530

    added comments

--- heuristics/dice_sorensen_cofficient.go
+++ heuristics/dice_sorensen_cofficient.go
@@ -2,12 +2,26 @@ package heuristics
 
 import "fuzzy/common"
 
+// Uses MultiSet Calculates the Dice-Sorensen coefficient for the given strings.
+//
+// Time Complexity: O(n + m)
+// Space Complexity: O(1) = 256 * sizeof(int)
+//
+// The Dice-Sorensen coefficient measures similarity between two strings using the following formula:
+// Dice-Sorensen Coefficient = 2 * IntersectionCount(a, b) / (len(a) + len(b))
 func DiceSorensenCoefficientCharacter[F common.FloatType, A common.StringLike, B common.StringLike](a A, b B) F {
   // len(a) + len(b) is ok as the sum cant exceed the native integer type anyways
   // 2 * IntersectionCount(a, b) is ok for the same reason
   return F(2 * common.IntersectionCharacterCount(a, b)) / F(uint(len(a)) + uint(len(b)))
 }
 
+// Uses Bigram Set Calculates the Dice-Sorensen coefficient for the given strings.
+//
+// Time Complexity: O(n + m)
+// Space Complexity: O(1) = 8kb
+//
+// The Dice-Sorensen coefficient measures similarity between two strings using the following formula:
+// Dice-Sorensen Coefficient = 2 * IntersectionCount(a, b) / (len(a) + len(b))
 func DiceSorensenCoefficientBigram[F common.FloatType, A common.StringLike, B common.StringLike](a A, b B) F {
   // len(a) + len(b) is ok as the sum cant exceed the native integer type anyways
   // 2 * IntersectionCount(a, b) is ok for the same reason
--- heuristics/jaccard_cofficient.go
+++ heuristics/jaccard_cofficient.go
@@ -2,6 +2,13 @@ package heuristics
 
 import "fuzzy/common"
 
+// Uses MultiSet Calculates the Jaccard coefficient for the given strings.
+//
+// Time Complexity: O(n + m)
+// Space Complexity: O(1) = 256 * sizeof(int)
+//
+// The Jaccard coefficient measures similarity between two strings using the following formula:
+// Jaccard Coefficient = IntersectionCount(a, b) / (len(a) + len(b) - IntersectionCount(a, b))
 func JaccardCoefficientCharacter[F common.FloatType, A common.StringLike, B common.StringLike](a A, b B) F {
   // len(a) + len(b) is ok as the sum cant exceed the native integer type anyways
   // 2 * IntersectionCount(a, b) is ok for the same reason
@@ -9,6 +16,13 @@ func JaccardCoefficientCharacter[F common.FloatType, A common.StringLike, B comm
   return F(intersection) / F(uint(len(a)) + uint(len(b)) - intersection)
 }
 
+// Uses Bigram Set Calculates the Jaccard coefficient for the given strings.
+//
+// Time Complexity: O(n + m)
+// Space Complexity: O(1) = 8kb
+//
+// The Jaccard coefficient measures similarity between two strings using the following formula:
+// Jaccard Coefficient = IntersectionCount(a, b) / (len(a) + len(b) - IntersectionCount(a, b))
 func JaccardCoefficientBigram[F common.FloatType, A common.StringLike, B common.StringLike](a A, b B) F {
   // len(a) + len(b) is ok as the sum cant exceed the native integer type anyways
   // 2 * IntersectionCount(a, b) is ok for the same reason
