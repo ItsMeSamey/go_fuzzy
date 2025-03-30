74dc1bebcf33e893f82554ab8f2f6cf363ef9679
ItsMeSamey <sameychain5041@gmail.com>
2025-03-30 12:02:05 +0530

    updated ascii filter

--- transformers/unicode_normalize.go
+++ transformers/unicode_normalize.go
@@ -1,9 +1,9 @@
 package transformers
 
 import (
+  "golang.org/x/text/runes"
   "golang.org/x/text/transform"
   "golang.org/x/text/unicode/norm"
-  "golang.org/x/text/encoding/charmap"
 )
 
 // Normalize Unicode to NFKD (decomposed form).
@@ -11,8 +11,10 @@ func UnicodeNormalize() transform.Transformer {
   return norm.NFKD
 }
 
-// Convert Unicode to ASCII, removing any characters that cannot be represented in ASCII.
-func UnicodeToAscii() transform.Transformer {
-  return transform.Chain(charmap.ISO8859_1.NewEncoder(), transform.RemoveFunc(func(r rune) bool { return r > 127 }))
+type asciiFilter struct {}
+func (asciiFilter) Contains(r rune) bool { return r >= 128 }
+// Removes any characters that cannot be represented in ASCII.
+func AsciiFilter() transform.Transformer {
+  return runes.Remove(asciiFilter{})
 }
 
