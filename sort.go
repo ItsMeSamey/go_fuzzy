865168323df6a5446854dbc493ddb28fc0fdecf1
ItsMeSamey <sameychain5041@gmail.com>
2025-03-30 12:59:25 +0530

    implemented threshold

--- sort.go
+++ sort.go
@@ -51,44 +51,72 @@ type SwapperInterface[A common.StringLike] interface {
 }
 
 type sortAnyType[F common.FloatType, A common.StringLike] struct {
+  len     int
   swapper SwapperInterface[A]
   scores  []F
 }
-func (s *sortAnyType[F, A]) Len() int { return s.swapper.Len() }
+func (s *sortAnyType[F, A]) Len() int { return s.len }
+func (s *sortAnyType[F, A]) SetLen(i int) { s.len = i }
 func (s *sortAnyType[F, A]) Less(i, j int) bool { return s.scores[i] < s.scores[j] }
 func (s *sortAnyType[F, A]) Swap(i, j int) {
   s.swapper.Swap(i, j)
   s.scores[i], s.scores[j] = s.scores[j], s.scores[i]
 }
+func (s *sortAnyType[F, A]) Score(i int) F { return s.scores[i] }
 
 // Sorts the `swapper` in place.
 func (sorter Sorter[F, A, B]) SortAny(swapper SwapperInterface[A], target B) int {
   return sorter.sort(&sortAnyType[F, A]{
+    len:     swapper.Len(),
     swapper: swapper,
     scores:  sorter.ScoreAny(swapper, target, ),
   })
 }
 
 type sortType[F common.FloatType, A common.StringLike] struct {
-  array    []A
-  scores   []F
+  len    int
+  array  []A
+  scores []F
 }
-func (s *sortType[F, A]) Len() int { return len(s.array) }
+func (s *sortType[F, A]) Len() int { return s.len }
+func (s *sortType[F, A]) SetLen(i int) { s.len = i }
 func (s *sortType[F, A]) Less(i, j int) bool { return s.scores[i] < s.scores[j] }
 func (s *sortType[F, A]) Swap(i, j int) {
   s.array[i], s.array[j] = s.array[j], s.array[i]
   s.scores[i], s.scores[j] = s.scores[j], s.scores[i]
 }
+func (s *sortType[F, A]) Score(i int) F { return s.scores[i] }
 
 // Sorts the array in place, returns the number of elements that are in the output
 func (sorter Sorter[F, A, B]) Sort(array []A, target B) int {
   return sorter.sort(&sortType[F, A]{
+    len:    len(array),
     array:  array,
     scores: sorter.Score(array, target, ),
   })
 }
 
-func (sorter Sorter[F, A, B]) sort(data sort.Interface) int {
-  return 0
+type sortInterface[F common.FloatType, A common.StringLike] interface {
+  Len() int
+  SetLen(i int)
+  Less(i, j int) bool
+  Swap(i, j int)
+  Score(i int) F
+}
+func (sorter Sorter[F, A, B]) sort(data sortInterface[F, A]) int {
+  below := 0
+  for below < data.Len() && data.Score(below) >= sorter.Threshold { below += 1 }
+
+  for i := below; i < data.Len(); i += 1 {
+    if data.Score(i) < sorter.Threshold { continue }
+    data.Swap(i, below)
+    below += 1
+  }
+
+  data.SetLen(below)
+
+  sort.Sort(data)
+
+  return below
 }
 
