44fbf2983b472f252b84f7ea692866bf9bc7c128
ItsMeSamey <sameychain5041@gmail.com>
2025-03-30 19:01:38 +0530

    made interface nicer

--- sort.go
+++ sort.go
@@ -8,12 +8,45 @@ import (
   "golang.org/x/text/transform"
 )
 
-type Scorer[F common.FloatType, A common.StringLike, B common.StringLike] struct {
-  // Returns a score Between 0 and 1 for the given pair of A and B.
-  ScoreFn func(a, b []byte) F
+type AccessorInterface[A common.StringLike] interface {
+  // Len is the number of elements in the collection.
+  Len() int
+  // Get i'th element in the collection.
+  Get(i int) A
+}
 
-  // Transformer
-  Transformer transform.Transformer
+type SwapperInterface[A common.StringLike] interface {
+  AccessorInterface[A]
+  Swap(i, j int)
+}
+
+type SwapperArrayInterface[T any, A common.StringLike] interface {
+  SwapperInterface[A]
+  Array() []T
+}
+
+type sortableArray[A common.StringLike] []A
+func (s sortableArray[A]) Len() int { return len(s) }
+func (s sortableArray[A]) Get(i int) A { return s[i] }
+func (s sortableArray[A]) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortableArray[A]) Array() []A { return s }
+
+func ToSwapperArray[A common.StringLike](array []A) SwapperArrayInterface[A, A] {
+  return sortableArray[A](array)
+}
+
+type sortableType[T any, A common.StringLike] struct {
+  array []T
+  get   func (t T) A
+}
+func (s sortableType[T, A]) Len() int { return len(s.array) }
+func (s sortableType[T, A]) Get(i int) A { return s.get(s.array[i]) }
+func (s sortableType[T, A]) Swap(i, j int) { s.array[i], s.array[j] = s.array[j], s.array[i] }
+func (s sortableType[T, A]) Array() []T { return s.array }
+
+func ToSwapper[T any, A common.StringLike](array []T, get func (t T) A) SwapperArrayInterface[T, A] {
+  if get == nil { panic("get must be set") }
+  return sortableType[T, A]{array, get}
 }
 
 func transformStringLike[A common.StringLike](t transform.Transformer, a A) []byte {
@@ -24,13 +57,13 @@ func transformStringLike[A common.StringLike](t transform.Transformer, a A) []by
   return out
 }
 
-type AccessorInterface[A common.StringLike] interface {
-  // Len is the number of elements in the collection.
-  Len() int
-  // Get i'th element in the collection.
-  Get(i int) A
-}
+type Scorer[F common.FloatType, A common.StringLike, B common.StringLike] struct {
+  // Returns a score Between 0 and 1 for the given pair of A and B.
+  ScoreFn func(a, b []byte) F
 
+  // Transformer
+  Transformer transform.Transformer
+}
 // Give an array of scores for all the elements in the `accessor` w.r.t. the `target`.
 func (sorter Scorer[F, A, B]) ScoreAny(accessor AccessorInterface[A], target B) (out []F) {
   out = make([]F, accessor.Len())
@@ -38,13 +71,9 @@ func (sorter Scorer[F, A, B]) ScoreAny(accessor AccessorInterface[A], target B) 
   for i := range accessor.Len() { out[i] = sorter.ScoreFn(transformStringLike(sorter.Transformer, accessor.Get(i)), byteTarget) }
   return
 }
-
 // Give an array of scores for all the elements in the `array` w.r.t. the `target`.
 func (sorter Scorer[F, A, B]) Score(array []A, target B) (out []F) {
-  out = make([]F, len(array))
-  byteTarget := transformStringLike(sorter.Transformer, target)
-  for i, a := range array { out[i] = sorter.ScoreFn(transformStringLike(sorter.Transformer, a), byteTarget) }
-  return
+  return sorter.ScoreAny(ToSwapperArray(array), target)
 }
 
 // A Struct used to sort a collection of elements.
@@ -55,12 +84,6 @@ type Sorter[F common.FloatType, A common.StringLike, B common.StringLike] struct
   // When this is 0, no threshold is applied
   Threshold F
 }
-
-type SwapperInterface[A common.StringLike] interface {
-  AccessorInterface[A]
-  Swap(i, j int)
-}
-
 type sortAnyType[F common.FloatType, A common.StringLike] struct {
   len     int
   swapper SwapperInterface[A]
@@ -80,30 +103,16 @@ func (sorter Sorter[F, A, B]) SortAny(swapper SwapperInterface[A], target B) int
   return sorter.sort(&sortAnyType[F, A]{
     len:     swapper.Len(),
     swapper: swapper,
-    scores:  sorter.ScoreAny(swapper, target, ),
+    scores:  sorter.ScoreAny(swapper, target),
   })
 }
 
-type sortType[F common.FloatType, A common.StringLike] struct {
-  len    int
-  array  []A
-  scores []F
-}
-func (s *sortType[F, A]) Len() int { return s.len }
-func (s *sortType[F, A]) SetLen(i int) { s.len = i }
-func (s *sortType[F, A]) Less(i, j int) bool { return s.scores[i] > s.scores[j] }
-func (s *sortType[F, A]) Swap(i, j int) {
-  s.array[i], s.array[j] = s.array[j], s.array[i]
-  s.scores[i], s.scores[j] = s.scores[j], s.scores[i]
-}
-func (s *sortType[F, A]) Score(i int) F { return s.scores[i] }
-
 // Sorts the array in place, returns the number of elements that are in the output
 func (sorter Sorter[F, A, B]) Sort(array []A, target B) int {
-  return sorter.sort(&sortType[F, A]{
+  return sorter.sort(&sortAnyType[F, A]{
     len:    len(array),
-    array:  array,
-    scores: sorter.Score(array, target, ),
+    swapper: ToSwapperArray(array),
+    scores: sorter.Score(array, target),
   })
 }
 
