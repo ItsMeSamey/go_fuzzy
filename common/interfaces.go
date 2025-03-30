095f75bf8a7e927ae7ae7059cc60b0fe12f8f691
ItsMeSamey <sameychain5041@gmail.com>
2025-03-30 11:54:12 +0530

    added ByteWriter for ttransformers

--- common/interfaces.go
+++ common/interfaces.go
@@ -3,7 +3,6 @@ package common
 type Number interface {
   int8 | int16 | int32 | int64 | float32 | float64 | int
 }
-
 func Abs[T Number](num T) T {
   if num < 0 { return -num }
   return num
@@ -15,5 +14,18 @@ type FloatType interface {
 
 type StringLike interface {
   string | []byte
+}
+
+type ByteWriter struct {
+  Buf []byte
+  At  int
+}
+func (w *ByteWriter) Write(p []byte) (n int, err error) {
+  copy(w.Buf[w.At:], p)
+  if len(w.Buf) < w.At + len(p) {
+    w.Buf = append(w.Buf, p[len(w.Buf)-w.At:]...)
+  }
+  w.At += len(p)
+  return len(p), nil
 }
 
